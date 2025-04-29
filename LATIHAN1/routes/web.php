751a5e6802c8e34e6d4957dbd605e5a39f527aa6
Zanesennn <Zanesen_2327240066@mhs.mdp.ac.id>
2025-04-29 20:29:21 +0700

    LATIHAN

--- LATIHAN1/routes/web.php
+++ LATIHAN1/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Home', function () {
+    return view('beranda',
+    [
+        'name'=> 'Zanesen',
+        'email' => 'Zanesen_2327240066@mhs.mdp.ac.id',
+        'alamat'=> 'Lorong Kiecong No 806'
+    ]);
+});
+
+Route::get('/berita/{id}/{judul}', function ($id,$judul) {
+    return view('berita',['id'=> $id,'judul' => $judul]);
+});
