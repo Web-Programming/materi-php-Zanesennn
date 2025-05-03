fac4ec261a7758e35ef80f66db01ea1891220462
Josannn <Zanesen_2327240066@mhs.mdp.ac.id>
2025-05-03 18:34:16 +0700

    Latihan1

--- LATIHAN1/routes/web.php
+++ LATIHAN1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +19,5 @@ Route::get('/Home', function () {
 Route::get('/berita/{id}/{judul?}', function ($id,$judul= null) {
     return view('berita',['id'=> $id,'judul' => $judul]);
 });
+
+Route::get('/prodi/index', [ProdiController::class,'index']);
