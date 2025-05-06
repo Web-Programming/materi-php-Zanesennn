bd44a28a44b88fd18d724448bbee791dd293a0b8
20040815 <Zanesen_2327240066@mhs.mdp.ac.id>
2025-05-06 20:29:11 +0700

    latihan

--- LATIHAN1/database/seeders/DatabaseSeeder.php
+++ LATIHAN1/database/seeders/DatabaseSeeder.php
@@ -16,10 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        DB::table("users")->insert([
-            'name' => 'zanesen2',
-            'email' => 'Zanesen2@gmail.com',
-            'password' => Hash::make("password")
-        ]);
+       // DB::table("users")->insert([
+        //    'name' => 'zanesen2',
+         //   'email' => 'Zanesen2@gmail.com',
+          //  'password' => Hash::make("password")
+        //]);
+
+         DB::table("users")
+         ->where("id",1)
+         ->update([
+            'password' => Hash::make("123456")
+         ]);
     }
 }
