715570bafcefc77be597523ac221d6cf24983267
Zanesennn <Zanesen_2327240066@mhs.mdp.ac.id>
2025-06-12 09:35:43 +0700

    Latihan

--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -1,27 +1,56 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\CekLogin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'login']);
 
-Route::get('/home', function(){
-    return view('beranda', 
-    [
-            'name' => 'Nur Rachmat',
-            'email' => 'nurrachmat@gmail.com',
-            'alamat' => 'Palembang'
-        ]
-    );
-});
+// Route::get('/home', function(){
+//     return view('beranda', 
+//     [
+//             'name' => 'Nur Rachmat',
+//             'email' => 'nurrachmat@gmail.com',
+//             'alamat' => 'Palembang'
+//         ]
+//     );
+// });
 
-Route::get('/berita/{id}/{judul?}', function($id, $judul = null){
-    return view('berita', ['id' => $id, 'judul' => $judul]);
-});
+// Route::get('/berita/{id}/{judul?}', function($id, $judul = null){
+//     return view('berita', ['id' => $id, 'judul' => $judul]);
+// });
 
 //membuat route ke halam prodi index melalui controller ProdiController
 //Route::get('/prodi/index', [ProdiController::class,'index']);
 
-Route::resource('prodi', ProdiController::class);
+//Route::resource('prodi', ProdiController::class);
+
+//Authentication
+Route::get("/login", [AuthController::class, 'login'])->name('login');
+Route::post("/login", [AuthController::class, 'do_login']);
+Route::get("/register", [AuthController::class, 'register']);
+Route::post("/register", [AuthController::class, 'do_register']);
+Route::get("/logout", [AuthController::class, 'logout']);
+
+// Route Grouping with Middleware
+Route::group(['middleware' => ['auth']], function(){
+
+    Route::get("/admin", [AdminController::class, 'index'])
+        ->middleware(CekLogin::class.':admin');
+    Route::get("/user", [UserController::class, 'index'])
+        ->middleware(CekLogin::class.':user');
+
+    Route::prefix('admin')->group(function(){
+        Route::resource('prodi', ProdiController::class);
+        Route::resource('fakultas', FakultasController::class);
+    })->middleware(CekLogin::class.':admin');
+
+    Route::prefix('user')->group(function(){
+        Route::resource('prodi', ProdiController::class);
+    })->middleware(CekLogin::class.':user');
+
+});
